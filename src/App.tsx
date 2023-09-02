e3a35d4f96ccee962a2b51885534c35b06327647
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-09-02 03:40:58 -0300

    criando páginas e estabelecendo rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Layout from './pages/Layout';
+import Login from './pages/Login/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+
 function App() {
-  return '';
+  return (
+    <Routes>
+      <Route path="/" element={ <Layout /> }>
+        <Route index element={ <Login /> } />
+        <Route path="/search" element={ <Search /> } />
+        <Route path="/album/:id" element={ <Album /> } />
+        <Route path="/favorites" element={ <Favorites /> } />
+        <Route path="/profile" element={ <Profile /> } />
+        <Route path="/profile/edit" element={ <ProfileEdit /> } />
+      </Route>
+      <Route path="/*" element={ <NotFound /> } />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/ProfileEdit/index.tsx
@@ -0,0 +1,5 @@
+function ProfileEdit() {
+  return (<h1>Edit Profile </h1>);
+}
+
+export default ProfileEdit;
